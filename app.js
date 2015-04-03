10d9dab3de1ae43be01aa92c37584224dac24018
Kana Natsuno <dev@whileimautomaton.net>
2015-04-04 00:01:34 +0900

    Define some constants

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 angular.module('Colors', [])
 .service('GameEngine', function () {
+  var numberOfPlayers = 2;  // TODO: Make it customizable at runtime.
+  var N = 5;
+
   function makeInitialGameTree() {
     // TODO: Implement.
   }
