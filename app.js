a524de14d67e91f21ea237771480dd36dbc9ab8c
Kana Natsuno <dev@whileimautomaton.net>
2015-04-04 00:01:19 +0900

    Define GameEngine skeleton

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-angular.module('Colors', []);
+angular.module('Colors', [])
+.service('GameEngine', function () {
+  function makeInitialGameTree() {
+    // TODO: Implement.
+  }
+
+  return {
+    makeInitialGameTree: makeInitialGameTree
+  };
+});
 // vim: expandtab softtabstop=2 shiftwidth=2 foldmethod=marker
