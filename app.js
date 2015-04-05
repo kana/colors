abde5aac749afb8cbe7c4866a41b63218a326825
Kana Natsuno <dev@whileimautomaton.net>
2015-04-05 16:46:26 +0900

    Define makeCardSet

--- app.js
+++ app.js
@@ -38,6 +38,26 @@ angular.module('Colors', [])
     return {joker: true};
   }
 
+  function makeCardSet() {
+    var baseCards = [];
+    for (var i = 0; i < N; i++)
+      baseCards.push(makeCard(i, i));
+
+    var freeCards = [];
+    for (var x = 0; x < N; x++) {
+      for (var y = 0; y < N; y++) {
+        if (x != y)
+          freeCards.push(makeCard(x, y));
+      }
+    }
+    freeCards.push(makeJoker());
+
+    return {
+      baseCards: baseCards,
+      freeCards: freeCards
+    };
+  }
+
   function makeInitialGameTree() {
     // TODO: Implement.
   }
